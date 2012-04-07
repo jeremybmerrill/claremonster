20872139ed5ef48ee72c91f60d4be308f287d861
Jeremy B. Merrill <jeremy@jeremybmerrill.com>
2012-04-07 03:05:14 -0700

    chaning index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Claremonster::Application.routes.draw do
 
-  #root to: 'wishes#index'
-  root to: 'static_pages#home'
+  root to: 'wishes#index'
+  #root to: 'static_pages#home'
 	 
   #get "static_pages/home"
   match '/about', to: 'static_pages#help'
