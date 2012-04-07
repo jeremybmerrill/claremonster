33c81e7c5f129b9ccd7ef033cb784f739168819b
Kimberly Merrill <kmerrill@cs227-13.cs.pomona.edu>
2012-04-07 05:42:16 -0700

    upvote front page

--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -3,7 +3,6 @@ class UpvotesController < ApplicationController
   def create
     @wish = Wish.find(params[:wish_id])
    	if @upvote = @wish.upvotes.create(params[:upvote]).save
-
         redirect_to wish_path(@wish)
     else
     	redirect_to wish_path(@wish), notice: 'You must log in to upvote a wish.'
